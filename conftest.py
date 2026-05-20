85eb6f5c2638d4a8ea2a8d2c656c4fbfd9c36d7f
nssiwi19 <7m79nf6rgz@privaterelay.appleid.com>
2026-05-20 14:20:11 +0700

    test(conftest): isolate test database by forcing testing env globally

--- conftest.py
+++ conftest.py
@@ -1,5 +1,9 @@
 import os
 import sys
 
+# Thiết lập môi trường kiểm thử để cô lập hoàn toàn cơ sở dữ liệu vật lý
+os.environ["APP_ENV"] = "testing"
+
 # Thêm thư mục hiện tại vào sys.path để pytest tìm thấy e16_app
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+
